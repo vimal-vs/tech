b2a29739ca727fcd2a025c9cfdb53904fd291572
Vimal Vs <vimalchan24@gmail.com>
2023-03-09 23:20:26 +0530

    nav and routing implemented

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function (req, res, next) {
     res.render('home', { title: 'SRMIST ' });
 });
 
+/* GET events page. */
+router.get('/events', function (req, res, next) {
+    res.render('events', { title: 'SRMIST ' });
+});
+
+/* GET venue page. */
+router.get('/venue', function (req, res, next) {
+    res.render('venue', { title: 'SRMIST ' });
+});
+
+/* GET contact-us page. */
+router.get('/contact-us', function (req, res, next) {
+    res.render('contact-us', { title: 'SRMIST ' });
+});
+
 module.exports = router;
